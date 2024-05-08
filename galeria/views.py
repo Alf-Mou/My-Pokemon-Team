a2ef84378a53913e28cfaeb1401e138561c486b9
Alfredo Moura <alfred.mou99@gmail.com>
2024-05-08 20:10:11 -0300

    up

--- galeria/views.py
+++ galeria/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'galeria/index.html')
+
+def pokemon(request):
+    return render(request, 'galeria/pokemon.html')
